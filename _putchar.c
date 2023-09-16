403b0938eaa11106437437bbf84796ecf0672ef6
Abdul Rahman Benyi <yhungmhansuccessor@gmail.com>
2023-09-16 10:01:15 -0700

    Output function handled

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - prints a character to the standard output
+ * @c: The character to be passed
+ * Return: returns the character passed to the stdout
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
