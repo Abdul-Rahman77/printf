517cb37e883a6cc01565bb36f771fe1b0c848ceb
7budi <7budisan@gmail.com>
2023-09-18 00:39:19 -0700

    _strlen

--- _strlen.c
+++ _strlen.c
@@ -1,10 +1,16 @@
-
-
-int _strlen(char *str) {
+/**
+ * _strlen - countes the string length
+ * @str: The string to be counted
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *str)
+{
 	int i = 0;
-	while (str[i] != '\0') {
+
+	while (str[i] != '\0')
+	{
 		i++;
 	}
 	return (i);
 }
-
