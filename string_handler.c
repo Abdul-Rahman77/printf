534a192eb05199e8d8628b51cf99b4881adb0d18
7budi <7budisan@gmail.com>
2023-09-18 00:06:30 -0700

    s_handler

---
+++ string_handler.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int string_handler(va_list list, char buff[], int i) {
+	char *str = va_arg(list, char*);
+	char count = 0;
+
+	while (str[count] != '\0') {
+		buff[i + count] = str[count];
+		count++;
+	}
+	i += count;
+	return (i);
+}
