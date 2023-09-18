99e846919de951541af2b08333eef1981aad09e3
7budi <7budisan@gmail.com>
2023-09-18 00:08:51 -0700

    main.h

--- main.h
+++ main.h
@@ -1,7 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define BUFFER_SIZE 1024
+#include <unistd.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-
+void print_string(char *str);
+int _strlen(char *str);
+int char_handler(va_list list, char buff[], int i);
+int string_handler(va_list list, char buff[], int i);
+int int_handler(va_list list, char main_buff[], int i);
+int binary_handler(va_list list, char main_buff[], int i);
+int octal_handler(va_list list, char main_buff[], int i);
+int hexadecimal_x_handler(va_list list, char main_buff[], int i);
+int hexadecimal_X_handler(va_list list, char main_buff[], int i);
 #endif
