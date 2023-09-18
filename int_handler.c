6f51db7a9e15355500ef21123ce5821ea72ba591
7budi <7budisan@gmail.com>
2023-09-18 01:01:44 -0700

    int_handler.c

--- int_handler.c
+++ int_handler.c
@@ -4,13 +4,14 @@
  *
  *
  */
-
-int int_handler(va_list list, char main_buff[], int main_count) {
+int int_handler(va_list list, char main_buff[], int main_count)
+{
 	int value = va_arg(list, int);
 	char buffer[INT_BUFF];
 	int parse_count = 0;
 
-	if (value < 0) {
+	if (value < 0)
+	{
 		buffer[parse_count++] = '-';
 		value = -value;
 	}
@@ -20,17 +21,21 @@ int int_handler(va_list list, char main_buff[], int main_count) {
 		value = (value / 10);
 	} while (value > 0);
 
-	for (int i = 0, j = parse_count - 1; i < j; i++, j--) {
+	for (int i = 0, j = parse_count - 1; i < j; i++, j--)
+	{
 		if (buffer[i] == '-')
 			i++;
 		char swap = buffer[i];
+
 		buffer[i] = buffer[j];
 		buffer[j] = swap;
 	}
 	buffer[parse_count] = '\0';
 
 	int mini_parser = 0;
-	while (buffer[mini_parser] != '\0') {
+
+	while (buffer[mini_parser] != '\0')
+	{
 		main_buff[main_count + mini_parser] = buffer[mini_parser];
 		mini_parser++;
 	}
