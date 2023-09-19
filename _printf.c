eb7d1dcfe380a2cc794c452e2648692db845a757
Abdul Rahman <yhungmhansuccessor@gmail.com>
2023-09-19 11:42:24 +0000

    Other unnecessary files removed

--- _printf.c
+++ _printf.c
@@ -98,3 +98,29 @@ int char_handler(va_list list, char buff[], int i)
 	i++;
 	return (i);
 }
+
+#include "main.h"
+
+/**
+ * string_handler - handles the %X flag in the format string
+ * @list: the va_list variable for the printf
+ * @buff: represents the buffer storing all chars to be printed
+ * @i: keeps track of all the chars to be printed
+ * Return: returns the total chars to be printed
+ */
+
+
+int string_handler(va_list list, char buff[], int i)
+{
+	char *str = malloc(char *sizeof(va_arg(list, char*));
+	int count = 0;
+
+	while (str[count] != '\0')
+	{
+		buff[i + count] = str[count];
+		count++;
+	}
+	i += count;
+	free(str);
+	return (i);
+}
