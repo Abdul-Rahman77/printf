264d42c43372040cd9e402dc884adbc29617b5e5
Abdul Rahman <yhungmhansuccessor@gmail.com>
2023-09-19 12:02:22 +0000

    _putchar.c and _strlen.c combined to support_functions.c

--- support_functions.c
+++ support_functions.c
@@ -10,3 +10,19 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+ * _strlen - countes the string length
+ * @str: The string to be counted
+ * Return: the length of the string
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
