62e0b73913da5921d75d32d7e6bbff1c551dd56d
Abdul Rahman <yhungmhansuccessor@gmail.com>
2023-09-19 11:35:51 +0000

    char_handler added to _printf.c

--- _printf.c
+++ _printf.c
@@ -80,3 +80,21 @@ int check_specifier(const char *format, int i, int count_char,
 		count_char = hexadecimal_X_handler(get_list, temp_buff, count_char);
 	return (count_char);
 }
+
+#include "main.h"
+/**
+ * char_handler - can handle any character
+ * @list:naiming the variadic function
+ * @buff:any empty array to store the characters
+ * @i:countes the characters
+ *
+ * Return:the characters
+ */
+int char_handler(va_list list, char buff[], int i)
+{
+	char character = va_arg(list, int);
+
+	buff[i] = character;
+	i++;
+	return (i);
+}
